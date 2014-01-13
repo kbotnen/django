dc34ae8e63a5624d561737ab96cdd54eb2115ecf
Kristian Botnen <kristian@mbmedia.no>
2014-01-13 21:29:40 +0000

    commit before migrate first model

--- mbpolls/models.py
+++ mbpolls/models.py
@@ -7,5 +7,5 @@ class Poll(models.Model):
 
 class Choice(models.Model):
     poll = models.ForeignKey(Poll)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
+    choice_text = models.CharField('choice text', max_length=200)
+    votes = models.IntegerField('vote', default=0)
